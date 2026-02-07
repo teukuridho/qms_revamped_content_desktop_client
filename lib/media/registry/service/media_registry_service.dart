6da0251c1b57c5783e5994948d2a0af4b8ed239c
Teuku Ridho - MSI Laptop <teukuridho2@gmail.com>
2026-02-07 21:23:35 +0700

    wip(media): media registry service

---
+++ lib/media/registry/service/media_registry_service.dart
@@ -0,0 +1,9 @@
+import 'package:qms_revamped_content_desktop_client/core/database/app_database_manager.dart';
+import 'package:qms_revamped_content_desktop_client/core/event_manager/event_manager.dart';
+
+class MediaRegistryService {
+  late final AppDatabaseManager _appDatabaseManager;
+  late final EventManager _eventManager;
+
+
+}
