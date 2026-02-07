ea7c8ef024455c36f938a44c5565cf2d74324326
Teuku Ridho - Witech <teukuridho@witech.co.id>
2026-02-07 15:04:38 +0700

    feat(media): media server properties

---
+++ lib/media/server_properties/media_server_properties_form_view.dart
@@ -0,0 +1,13 @@
+import 'package:qms_revamped_content_desktop_client/core/server_properties/form/ui/view/server_properties_form_view.dart';
+import 'package:qms_revamped_content_desktop_client/core/server_properties/form/ui/view_model/server_properties_form_view_model.dart';
+import 'package:qms_revamped_content_desktop_client/core/server_properties/registry/service/server_properties_registry_service.dart';
+
+class MediaServerPropertiesFormView extends ServerPropertiesFormView {
+  MediaServerPropertiesFormView(
+      {super.key, required ServerPropertiesRegistryService registryService})
+      : super(viewModel: ServerPropertiesFormViewModel(
+      registryService: registryService,
+      serviceName: "media"
+  ));
+
+}
